6c4cce66c44abf455f88713e9996dba1313e0017
nakhim.chea <cheanakhimzzzz@gmail.com>
2022-11-07 10:27:18 +0700

    running dev

--- freqtrade/user_data/strategies/SmartTA.py
+++ freqtrade/user_data/strategies/SmartTA.py
@@ -32,41 +32,41 @@ class SmartTA(IStrategy):
 
     # ROI table:
     minimal_roi = {
-        "0": 0.117,
-        "1704": 0.081,
-        "3712": 0.051,
+        "0": 0.0585,
+        "1704": 0.0405,
+        "3712": 0.0255,
         "5605": 0
     }
 
     # Stoploss:
-    stoploss = -0.7
+    stoploss = -0.35
 
     # enable short
     can_short = True
 
     # Trailing stop:
     trailing_stop = True
-    trailing_stop_positive = 0.01
-    trailing_stop_positive_offset = 0.02
+    trailing_stop_positive = 0.005
+    trailing_stop_positive_offset = 0.01
     trailing_only_offset_is_reached = True
 
     timeframe = "4h"
 
     startup_candle_count = 18
 
-    buy_m1 = IntParameter(1, 7, default=1)
-    buy_m2 = IntParameter(1, 7, default=3)
-    buy_m3 = IntParameter(1, 7, default=4)
-    buy_p1 = IntParameter(7, 21, default=14)
-    buy_p2 = IntParameter(7, 21, default=10)
-    buy_p3 = IntParameter(7, 21, default=10)
+    buy_m1 = IntParameter(1, 7, default=4)
+    buy_m2 = IntParameter(1, 7, default=7)
+    buy_m3 = IntParameter(1, 7, default=1)
+    buy_p1 = IntParameter(7, 21, default=8)
+    buy_p2 = IntParameter(7, 21, default=9)
+    buy_p3 = IntParameter(7, 21, default=8)
 
     sell_m1 = IntParameter(1, 7, default=1)
     sell_m2 = IntParameter(1, 7, default=3)
-    sell_m3 = IntParameter(1, 7, default=4)
-    sell_p1 = IntParameter(7, 21, default=14)
-    sell_p2 = IntParameter(7, 21, default=10)
-    sell_p3 = IntParameter(7, 21, default=10)
+    sell_m3 = IntParameter(1, 7, default=6)
+    sell_p1 = IntParameter(7, 21, default=16)
+    sell_p2 = IntParameter(7, 21, default=18)
+    sell_p3 = IntParameter(7, 21, default=18)
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         for multiplier in self.buy_m1.range:
@@ -115,15 +115,23 @@ class SmartTA(IStrategy):
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
-            (dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "down")
+            ((dataframe[f"supertrend_1_buy_{self.buy_m1.value}_{self.buy_p1.value}"] == "down")
+            & (dataframe[f"supertrend_2_buy_{self.buy_m2.value}_{self.buy_p2.value}"] == "down")
+            & (dataframe[f"supertrend_3_buy_{self.buy_m3.value}_{self.buy_p3.value}"] == "down")
+            & (dataframe["volume"] > 0))
+            | ((dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "down")
             & (dataframe[f"supertrend_2_sell_{self.sell_m2.value}_{self.sell_p2.value}"] == "down")
-            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "down"),
+            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "down")),
             "exit_long"] = 1
 
         dataframe.loc[
-            (dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "up")
+            ((dataframe[f"supertrend_1_buy_{self.buy_m1.value}_{self.buy_p1.value}"] == "up")
+            & (dataframe[f"supertrend_2_buy_{self.buy_m2.value}_{self.buy_p2.value}"] == "up")
+            & (dataframe[f"supertrend_3_buy_{self.buy_m3.value}_{self.buy_p3.value}"] == "up")
+            & (dataframe["volume"] > 0))
+            | ((dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "up")
             & (dataframe[f"supertrend_2_sell_{self.sell_m2.value}_{self.sell_p2.value}"] == "up")
-            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "up"),
+            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "up")),
             "exit_short"] = 1
 
         return dataframe
@@ -132,7 +140,7 @@ class SmartTA(IStrategy):
                  proposed_leverage: float, max_leverage: float, entry_tag: Optional[str], side: str,
                  **kwargs) -> float:
 
-        return 2.0
+        return 1.0
 
     @staticmethod
     def supertrend(dataframe: DataFrame, multiplier, period):
@@ -170,17 +178,13 @@ class SmartTA(IStrategy):
         for i in range(period, len(df)):
             df[st].iat[i] = (
                 df["final_ub"].iat[i]
-                if df[st].iat[i - 1] == df["final_ub"].iat[i - 1]
-                and df["close"].iat[i] <= df["final_ub"].iat[i]
+                if df[st].iat[i - 1] == df["final_ub"].iat[i - 1] and df["close"].iat[i] <= df["final_ub"].iat[i]
                 else df["final_lb"].iat[i]
-                if df[st].iat[i - 1] == df["final_ub"].iat[i - 1]
-                and df["close"].iat[i] > df["final_ub"].iat[i]
+                if df[st].iat[i - 1] == df["final_ub"].iat[i - 1] and df["close"].iat[i] > df["final_ub"].iat[i]
                 else df["final_lb"].iat[i]
-                if df[st].iat[i - 1] == df["final_lb"].iat[i - 1]
-                and df["close"].iat[i] >= df["final_lb"].iat[i]
+                if df[st].iat[i - 1] == df["final_lb"].iat[i - 1] and df["close"].iat[i] >= df["final_lb"].iat[i]
                 else df["final_ub"].iat[i]
-                if df[st].iat[i - 1] == df["final_lb"].iat[i - 1]
-                and df["close"].iat[i] < df["final_lb"].iat[i]
+                if df[st].iat[i - 1] == df["final_lb"].iat[i - 1] and df["close"].iat[i] < df["final_lb"].iat[i]
                 else 0.00
             )
         # Mark the trend direction up/down
