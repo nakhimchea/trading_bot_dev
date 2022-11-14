d5b8d0d9eaff94ae97ebb085999d6421c33e5e61
nakhim.chea <cheanakhimzzzz@gmail.com>
2022-11-14 22:40:49 +0700

    Change ROI and stoploss

--- freqtrade/user_data/strategies/SmartTA.py
+++ freqtrade/user_data/strategies/SmartTA.py
@@ -5,6 +5,7 @@ from freqtrade.strategy import IStrategy, IntParameter
 from pandas import DataFrame
 import talib.abstract as ta
 import numpy as np
+import scipy
 
 
 class SmartTA(IStrategy):
@@ -32,14 +33,14 @@ class SmartTA(IStrategy):
 
     # ROI table:
     minimal_roi = {
-        "0": 0.0585,
-        "1704": 0.0405,
-        "3712": 0.0255,
+        "0": 0.04,
+        "1704": 0.03,
+        "3712": 0.018,
         "5605": 0
     }
 
     # Stoploss:
-    stoploss = -0.35
+    stoploss = -1
 
     # enable short
     can_short = True
@@ -51,7 +52,6 @@ class SmartTA(IStrategy):
     trailing_only_offset_is_reached = True
 
     timeframe = "4h"
-
     startup_candle_count = 18
 
     buy_m1 = IntParameter(1, 7, default=4)
@@ -69,6 +69,8 @@ class SmartTA(IStrategy):
     sell_p3 = IntParameter(7, 21, default=18)
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['peak_bottom'] = self.smartexit(dataframe)['peak_bottom']
+
         for multiplier in self.buy_m1.range:
             for period in self.buy_p1.range:
                 dataframe[f"supertrend_1_buy_{multiplier}_{period}"] = self.supertrend(dataframe, multiplier, period)["STX"]
@@ -96,11 +98,16 @@ class SmartTA(IStrategy):
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         dataframe.loc[
             (dataframe[f"supertrend_1_buy_{self.buy_m1.value}_{self.buy_p1.value}"] == "up")
             & (dataframe[f"supertrend_2_buy_{self.buy_m2.value}_{self.buy_p2.value}"] == "up")
             & (dataframe[f"supertrend_3_buy_{self.buy_m3.value}_{self.buy_p3.value}"] == "up")
+            & (dataframe['peak_bottom'] != 'P0')
+            & (dataframe['peak_bottom'] != 'P1')
+            & (dataframe['peak_bottom'] != 'P2')
+            & (dataframe['peak_bottom'] != 'P3')
+            & (dataframe['peak_bottom'] != 'P4')
+            & (dataframe['peak_bottom'] != 'P5')
             & (dataframe["volume"] > 0),
             "enter_long"] = 1
 
@@ -108,6 +115,12 @@ class SmartTA(IStrategy):
             (dataframe[f"supertrend_1_buy_{self.buy_m1.value}_{self.buy_p1.value}"] == "down")
             & (dataframe[f"supertrend_2_buy_{self.buy_m2.value}_{self.buy_p2.value}"] == "down")
             & (dataframe[f"supertrend_3_buy_{self.buy_m3.value}_{self.buy_p3.value}"] == "down")
+            & (dataframe['peak_bottom'] != 'B0')
+            & (dataframe['peak_bottom'] != 'B1')
+            & (dataframe['peak_bottom'] != 'B2')
+            & (dataframe['peak_bottom'] != 'B3')
+            & (dataframe['peak_bottom'] != 'B4')
+            & (dataframe['peak_bottom'] != 'B5')
             & (dataframe["volume"] > 0),
             "enter_short"] = 1
 
@@ -115,23 +128,15 @@ class SmartTA(IStrategy):
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
-            ((dataframe[f"supertrend_1_buy_{self.buy_m1.value}_{self.buy_p1.value}"] == "down")
-            & (dataframe[f"supertrend_2_buy_{self.buy_m2.value}_{self.buy_p2.value}"] == "down")
-            & (dataframe[f"supertrend_3_buy_{self.buy_m3.value}_{self.buy_p3.value}"] == "down")
-            & (dataframe["volume"] > 0))
-            | ((dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "down")
+            (dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "down")
             & (dataframe[f"supertrend_2_sell_{self.sell_m2.value}_{self.sell_p2.value}"] == "down")
-            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "down")),
+            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "down"),
             "exit_long"] = 1
 
         dataframe.loc[
-            ((dataframe[f"supertrend_1_buy_{self.buy_m1.value}_{self.buy_p1.value}"] == "up")
-            & (dataframe[f"supertrend_2_buy_{self.buy_m2.value}_{self.buy_p2.value}"] == "up")
-            & (dataframe[f"supertrend_3_buy_{self.buy_m3.value}_{self.buy_p3.value}"] == "up")
-            & (dataframe["volume"] > 0))
-            | ((dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "up")
+            (dataframe[f"supertrend_1_sell_{self.sell_m1.value}_{self.sell_p1.value}"] == "up")
             & (dataframe[f"supertrend_2_sell_{self.sell_m2.value}_{self.sell_p2.value}"] == "up")
-            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "up")),
+            & (dataframe[f"supertrend_3_sell_{self.sell_m3.value}_{self.sell_p3.value}"] == "up"),
             "exit_short"] = 1
 
         return dataframe
@@ -162,14 +167,12 @@ class SmartTA(IStrategy):
         for i in range(period, len(df)):
             df["final_ub"].iat[i] = (
                 df["basic_ub"].iat[i]
-                if df["basic_ub"].iat[i] < df["final_ub"].iat[i - 1]
-                or df["close"].iat[i - 1] > df["final_ub"].iat[i - 1]
+                if df["basic_ub"].iat[i] < df["final_ub"].iat[i - 1] or df["close"].iat[i - 1] > df["final_ub"].iat[i - 1]
                 else df["final_ub"].iat[i - 1]
             )
             df["final_lb"].iat[i] = (
                 df["basic_lb"].iat[i]
-                if df["basic_lb"].iat[i] > df["final_lb"].iat[i - 1]
-                or df["close"].iat[i - 1] < df["final_lb"].iat[i - 1]
+                if df["basic_lb"].iat[i] > df["final_lb"].iat[i - 1] or df["close"].iat[i - 1] < df["final_lb"].iat[i - 1]
                 else df["final_lb"].iat[i - 1]
             )
 
