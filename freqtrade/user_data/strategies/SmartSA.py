273f16b17638fdfef1ae7033f3a9a6d4302792c7
nakhim.chea <cheanakhimzzzz@gmail.com>
2023-01-05 11:35:39 +0700

    Thread hourly & Mockup JSON

--- freqtrade/user_data/strategies/SmartSA.py
+++ freqtrade/user_data/strategies/SmartSA.py
@@ -1,4 +1,20 @@
 # This is an integration of LunarCrush APIs to Freqtrade to execute spot market orders
+import time
+import threading
 
 
-print('Hello')
+def request_lunar_crush():
+    print("Do some work")
+    time.sleep(1)
+    print("Some work is done!")
+
+
+def main():
+    while True:
+        thr = threading.Thread(target=request_lunar_crush)
+        thr.start()
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
