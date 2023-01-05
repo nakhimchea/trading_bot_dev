4370d63f1b371ac1a904495a8059be5ae414b394
nakhim.chea <cheanakhimzzzz@gmail.com>
2023-01-05 13:57:55 +0700

    Request from mockup and save to JSON file

--- freqtrade/user_data/strategies/SmartSA.py
+++ freqtrade/user_data/strategies/SmartSA.py
@@ -1,19 +1,26 @@
 # This is an integration of LunarCrush APIs to Freqtrade to execute spot market orders
 import time
-import threading
+from threading import Thread
+import json
 
 
 def request_lunar_crush():
-    print("Do some work")
-    time.sleep(1)
-    print("Some work is done!")
+    data = json.load(open('lunarcrush.json'))
+    print(data["data"])
+
+    # Serializing json
+    json_object = json.dumps(data["data"], indent=2)
+
+    # Writing to json file
+    with open("lunarcrush_extraction.json", "w") as outfile:
+        outfile.write(json_object)
 
 
 def main():
     while True:
-        thr = threading.Thread(target=request_lunar_crush)
+        thr = Thread(target=request_lunar_crush)
         thr.start()
-        time.sleep(2)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
